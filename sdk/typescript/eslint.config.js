16c4178c9a6aabe857bd1aa0d02d97995f006c27
GitHub Action <action@github.com>
2026-05-24 08:21:28 +0000

    chore(release): 0.9.16 [skip ci]

--- sdk/typescript/eslint.config.js
+++ sdk/typescript/eslint.config.js
@@ -2,6 +2,9 @@ import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import nodeImport from "eslint-plugin-node-import";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
   plugins: {
@@ -11,4 +14,4 @@ export default defineConfig(eslint.configs.recommended, tseslint.configs.recomme
   rules: {
     "node-import/prefer-node-protocol": 2,
   },
-});
+});\n
